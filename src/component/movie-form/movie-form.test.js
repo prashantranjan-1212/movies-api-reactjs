15f2c7d2f4635a5145f79cb85011a69a0ba6a68d
prashantranjan-1212 <prashantranjan1212@gmail.com>
2023-11-07 09:54:28 +0530

    updating form submit value

--- src/component/movie-form/movie-form.test.js
+++ src/component/movie-form/movie-form.test.js
@@ -65,3 +65,129 @@ test("renders Movie Form component", () => {
   const movieFormSubmit = getByTestId("movie-form-submit");
   expect(movieFormSubmit).toBeInTheDocument();
 });
+
+test("Test Movie Form submit", () => {
+  const onSubmit = jest.fn();
+  const movieInfo = {
+    title: "Jawan",
+    releaseDate: "2023-02-26",
+    movieUrl: "https://google.com/movie",
+    rating: 8,
+    genre: "Action",
+    runtime: "120",
+    overview: "A Master Peice",
+  };
+
+  const { getByTestId } = render(
+    <MovieForm movieInfo={movieInfo} onSubmit={onSubmit} />
+  );
+
+  const movieFormConatiner = getByTestId("movie-form-container");
+  expect(movieFormConatiner).toBeInTheDocument();
+
+  const movieFormSubmit = getByTestId("movie-form-submit");
+  expect(movieFormSubmit).toBeInTheDocument();
+
+  fireEvent.click(movieFormSubmit);
+  expect(onSubmit).toHaveBeenCalledWith({
+    title: "Jawan",
+    releaseDate: "2023-02-26",
+    movieUrl: "https://google.com/movie",
+    rating: "8",
+    genre: "Action",
+    runtime: "120",
+    overview: "A Master Peice",
+  });
+});
+
+test("Test Form Data/Value Change", () => {
+  const onSubmit = jest.fn();
+  const movieInfo = {
+    title: "Jawan",
+    releaseDate: "2023-02-26",
+    movieUrl: "https://google.com/movie",
+    rating: 8,
+    genre: "Action",
+    runtime: "120",
+    overview: "A Master Peice",
+  };
+
+  const { getByTestId } = render(
+    <MovieForm movieInfo={movieInfo} onSubmit={onSubmit} />
+  );
+
+  const movieFormConatiner = getByTestId("movie-form-container");
+  expect(movieFormConatiner).toBeInTheDocument();
+
+  const movieFormSubmit = getByTestId("movie-form-submit");
+  expect(movieFormSubmit).toBeInTheDocument();
+
+  const titleInput = getByTestId("title-input");
+  fireEvent.change(titleInput, { target: { value: "Hero" } });
+
+  const releaseDateInput = getByTestId("release-date-input");
+  fireEvent.change(releaseDateInput, { target: { value: "2015-02-26" } });
+
+  const movieUrlInput = getByTestId("movie-url-input");
+  fireEvent.change(movieUrlInput, {
+    target: { value: "https://google.com/movie/video.mp4" },
+  });
+
+  const ratingInput = getByTestId("rating-input");
+  fireEvent.change(ratingInput, { target: { value: 5 } });
+
+  const genreSelect = getByTestId("genre-select");
+  fireEvent.change(genreSelect, { target: { value: "Comedy" } });
+
+  const runtimeInput = getByTestId("runtime-input");
+  fireEvent.change(runtimeInput, { target: { value: "90" } });
+
+  const overviewTextArea = getByTestId("overview-textarea");
+  fireEvent.change(overviewTextArea, {
+    target: { value: "A truly shitty movie of the  century" },
+  });
+
+  fireEvent.click(movieFormSubmit);
+  expect(onSubmit).toHaveBeenCalledWith({
+    title: "Hero",
+    releaseDate: "2015-02-26",
+    movieUrl: "https://google.com/movie/video.mp4",
+    rating: "5",
+    genre: "Comedy",
+    runtime: "90",
+    overview: "A truly shitty movie of the  century",
+  });
+});
+
+test("Test for Valid Rating Value", () => {
+  const onSubmit = jest.fn();
+  const movieInfo = {
+    title: "Jawan",
+    releaseDate: "2023-02-26",
+    movieUrl: "https://google.com/movie",
+    rating: 8,
+    genre: "Action",
+    runtime: "120",
+    overview: "A Master Peice",
+  };
+
+  const { getByTestId } = render(
+    <MovieForm movieInfo={movieInfo} onSubmit={onSubmit} />
+  );
+
+  const movieFormConatiner = getByTestId("movie-form-container");
+  expect(movieFormConatiner).toBeInTheDocument();
+  const movieFormSubmit = getByTestId("movie-form-submit");
+  expect(movieFormSubmit).toBeInTheDocument();
+
+  const ratingInput = getByTestId("rating-input");
+  fireEvent.change(ratingInput, { target: { value: -1 } });
+
+  fireEvent.click(movieFormSubmit);
+  expect(onSubmit).not.toHaveBeenCalledWith();
+
+  fireEvent.change(ratingInput, { target: { value: 11 } });
+
+  fireEvent.click(movieFormSubmit);
+  expect(onSubmit).not.toHaveBeenCalledWith();
+});
