3a9835ea1e8e2afa4183f9ba8e809ffe9ed0e7b0
prashantranjan-1212 <prashantranjan1212@gmail.com>
2023-12-08 09:53:48 +0530

    updating form modules.

--- src/component/movie-form/movie-form.component.js
+++ src/component/movie-form/movie-form.component.js
@@ -2,10 +2,13 @@ import "./movie-form.style.scss";
 
 import { useState } from "react";
 import PropTypes from "prop-types";
-import ButtonLink from "../button-link/button-link.component";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const MovieForm = ({ movieInfo, onSubmit }) => {
+	const [register, handleSubmit] = useForm();
+	const [data, setData] = useState("");
+
 	const [title, setTitle] = useState(movieInfo.title || "");
 	const [releaseDate, setReleaseDate] = useState(movieInfo.releaseDate || "");
 	const [movieUrl, setMovieUrl] = useState(movieInfo.movieUrl || "");
@@ -26,31 +29,40 @@ const MovieForm = ({ movieInfo, onSubmit }) => {
 			className="movie-form-container"
 			data-testid="movie-form-container"
 		>
-			<form onSubmit={handleMovieSubmit}>
+			<form
+				onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+			>
 				<label data-testid="title-label">TITLE</label>
 				<input
 					data-testid="title-input"
 					type="text"
 					name="title"
-					value={title}
 					placeholder="Movie Title "
-					onChange={(event) => setTitle(event.target.value)}
+					{...register("title", {
+						required: true,
+						onChange: (event) => setTitle(event.target.value),
+					})}
 				></input>
 				<label data-testid="release-date-label">RELEASE DATE</label>
 				<input
 					data-testid="release-date-input"
 					type="date"
 					name="releaseDate"
-					value={releaseDate}
-					onChange={(event) => setReleaseDate(event.target.value)}
+					{...register("releaseDate", {
+						required: true,
+						valueAsDate: true,
+						onChange: (event) => setReleaseDate(event.target.value),
+					})}
 				></input>
 				<label data-testid="movie-url-label">MOVIE URL</label>
 				<input
 					data-testid="movie-url-input"
 					type="url"
 					name="movieUrl"
-					value={movieUrl}
-					onChange={(event) => setMovieUrl(event.target.value)}
+					{...register("movieUrl", {
+						required: true,
+						onChange: (event) => setMovieUrl(event.target.value),
+					})}
 				></input>
 				<label data-testid="rating-label">RATING</label>
 				<input
@@ -59,15 +71,19 @@ const MovieForm = ({ movieInfo, onSubmit }) => {
 					name="rating"
 					min="0"
 					max="10"
-					value={rating}
-					onChange={(event) => setRating(event.target.value)}
+					{...register("rating", {
+						required: true,
+						onChange: (event) => setRating(event.target.value),
+					})}
 				></input>
 				<label data-testid="genre-label">GENRE</label>
 				<select
 					data-testid="genre-select"
-					value={genre}
 					name="genre"
-					onChange={(event) => setGenre(event.target.value)}
+					{...register("genre", {
+						required: true,
+						onChange: (event) => setGenre(event.target.value),
+					})}
 				>
 					<option value="Comedy">Comedy</option>
 					<option value="Fantasy">Fantasy</option>
@@ -82,15 +98,19 @@ const MovieForm = ({ movieInfo, onSubmit }) => {
 					data-testid="runtime-input"
 					type="text"
 					name="runtime"
-					value={runtime}
-					onChange={(event) => setRuntime(event.target.value)}
+					{...register("runtime", {
+						required: true,
+						onChange: (event) => setRuntime(event.target.value),
+					})}
 				></input>
 				<label data-testid="overview-label">OVERVIEW</label>
 				<textarea
 					data-testid="overview-textarea"
-					value={overview}
 					name="overview"
-					onChange={(event) => setOverview(event.target.value)}
+					{...register("overview", {
+						required: true,
+						onChange: (event) => setOverview(event.target.value),
+					})}
 				/>
 				<Link
 					className="form-submit"
